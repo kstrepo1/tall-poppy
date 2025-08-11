9e90f0705b722db19a90591fc1ffb0584abed279
kstrepo1 <kail.terepai@griffithuni.edu.au>
2025-08-11 20:14:32 +1000

    background update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,21 @@ import Image from "next/image";
 export default function Home() {
   return (
 
-    <header>
-        <Image
-        alt={"Tall Poppy Studios"}
-        src={"/TPS_logo.png"}
-        height={100}
-        width={500}
-        />
-    </header>
+    <div style={{ ...styles.head, background: "#000000" }}>
+      <Image 
+      style={{ background: "#000000" }}
+      alt={"Tall Poppy Studios"}
+      src={"/TPS_logo.png"}
+      height={100}
+      width={500}
+      />
+    </div>
 
   );
+}
+
+var styles={
+  head: {
+    background: "#000000"
+  }
 }
