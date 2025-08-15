6893304804a7048d928084b05df0edeeec07279d
kstrepo1 <kail.terepai@griffithuni.edu.au>
2025-08-16 09:58:47 +1000

    logo background and change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,22 +3,51 @@ import Image from "next/image";
 
 export default function Home() {
   return (
+<>
 
-    <div style={{ ...styles.head, background: "#000000" }}>
-      <Image 
-      style={{ background: "#000000" }}
+  <div
+    style={{
+      ...styles.head,
+    } as React.CSSProperties}
+  >
+    <Image
       alt={"Tall Poppy Studios"}
-      src={"/TPS_logo.png"}
+      src={"/logo-2.png"}
       height={100}
       width={500}
+      priority
+    />
+  </div>
+    
+    <div style={styles.background}>
+      <Image 
+      style={{objectFit: "cover"}}
+      alt={"Background Gif"}
+      src={"/background.gif"}
+      fill={true}
+      
       />
     </div>
 
+ 
+  </>
   );
 }
 
 const styles={
   head: {
-    background: "#000000"
+    
+    position: "absolute",
+    zIndex: 2,
+      width: "100%",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      top: 0,
+      left: 8,
+  },
+  background: {
+    
+
   }
 }
