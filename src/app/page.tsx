a69da6b08d703e87a60cc446bc3ab22323fa7e53
kstrepo1 <kail.terepai@griffithuni.edu.au>
2025-08-16 10:12:32 +1000

    loader

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,48 @@
+"use client"
 import Image from "next/image";
+import { useState } from "react";
+
 
 
 export default function Home() {
+  const [bgLoaded, setbgLoaded] = useState(false);
+  
   return (
 <>
 
-  <div
-    style={{
-      ...styles.head,
-    } as React.CSSProperties}
-  >
-    <Image
-      alt={"Tall Poppy Studios"}
-      src={"/logo-2.png"}
-      height={100}
-      width={500}
-      priority
-    />
-  </div>
+  {!bgLoaded ? (
+    <div
+      style={{
+        ...styles.head,
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        background: "#000",
+        color: "#fff",
+        fontSize: 32,
+        zIndex: 10,
+      } as React.CSSProperties}
+    >
+      
+
+      <div className="loader"></div>
+
+    </div>
+  ) : (
+    <div
+      style={{
+        ...styles.head,
+      } as React.CSSProperties}
+    >
+      <Image
+        alt={"Tall Poppy Studios"}
+        src={"/logo-2.png"}
+        height={100}
+        width={500}
+        priority
+      />
+    </div>
+  )}
     
     <div style={styles.background}>
       <Image 
@@ -25,7 +50,7 @@ export default function Home() {
       alt={"Background Gif"}
       src={"/background.gif"}
       fill={true}
-      
+      onLoad={()=>setbgLoaded(true)}
       />
     </div>
 
