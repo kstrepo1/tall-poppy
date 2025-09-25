62d2268c8bd474bb8e68f849854d99ba73522a54
kstrepo1 <kail.terepai@griffithuni.edu.au>
2025-09-25 21:14:13 +1000

    icon updates

--- src/app/coreHeader.tsx
+++ src/app/coreHeader.tsx
@@ -10,7 +10,10 @@ export default function Header() {
     return(
         <><div id="headerElements">
             <div id="headerLogo">
+                <Link href="home">
                 <Image src={"/tps-long-ff.png"} width={400} height={50} alt="Tall Poppy Studios Logo"/>
+                </Link>
+                
                 
             </div>
                 
