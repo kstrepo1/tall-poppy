030a619037d9d702f3830e60fc08f269b7f54086
kstrepo1 <kail.terepai@griffithuni.edu.au>
2025-10-19 23:24:38 +1000

    FBMJ1

---
+++ src/app/gallery/FBMJ.tsx
@@ -0,0 +1,86 @@
+"use client"
+import Image from "next/image"
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { ScrambleTextPlugin } from "gsap/ScrambleTextPlugin";
+gsap.registerPlugin(useGSAP);
+gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrambleTextPlugin);
+
+
+export default function FBMJ(){
+
+    useGSAP(() => { 
+        gsap.to('.brandHeader', {
+        scrollTrigger: '.brandHeader', // start animation when ".box" enters the viewport
+        x: 0
+        });
+
+        gsap.to('#brandHeaderFBMJ', {
+        duration: 1, 
+        scrambleText: {
+            text: "BOWQUET by FBMJ",
+            chars: "XO"
+        }
+        });
+
+        gsap.to('#brandServicesFBMJ', {
+        duration: 2,
+        delay: 0.5,
+        scrambleText:{
+            text: "PHOTOGRAPHY, CREATIVE DIRECTION",
+            chars: "XO",
+        } 
+        
+        });
+
+    });
+
+
+
+    const images1 = [
+        {id:0, src:"/gallery/fbmj/FBMJ1 004_ whisper honey.jpg", alt: "FBMJ", imageHeader:"WHISPER HONEY", imageText:""},
+        {id:1, src:"/gallery/fbmj/FBMJ1 005_ chemistry.jpg", alt: "FBMJ", imageHeader:"CHEMISTRY", imageText:""},
+        {id:2, src:"/gallery/fbmj/FBMJ1 009_ falling ( love again ).jpg", alt: "FBMJ", imageHeader:"FALLING", imageText:""},
+        {id:3, src:"/gallery/fbmj/FBMJ1 010_ out of control.jpg", alt: "FBMJ", imageHeader:"OUT OF CONTROL", imageText:""},
+        {id:4, src:"/gallery/fbmj/FBMJ1 COMPLIATION.jpg", alt: "FBMJ", imageHeader:"COMPLIATIONY", imageText:""},
+        {id:5, src:"/gallery/fbmj/FBMJ1 WHISPER HONEY 1.jpg", alt: "FBMJ", imageHeader:"WHISPER HONEY", imageText:""},
+        {id:6, src:"/gallery/fbmj/FBMJ1 WHISPER HONEY 2.jpg", alt: "FBMJ", imageHeader:"WHISPER HONEY", imageText:""},
+        {id:7, src:"/gallery/fbmj/FMBJ1 005_ chemistry.jpg", alt: "FBMJ", imageHeader:"CHEMISTRY", imageText:""},
+    ]
+
+    return(
+        <>
+        <hr className="white-line"/>
+        <div className="headerBox">
+            <div className="brandHeader" id="brandHeaderFBMJ"></div>
+            <div className="brandServices" id="brandServicesFBMJ"></div>
+        </div>
+        
+        <div className="galleryImages" style={{ display: 'flex', flexDirection: 'row', gap: '10px', flexWrap: 'wrap', justifyContent:"center"}}>
+        {images1.map((image) => (
+            <div key={image.id}>
+            <Image 
+            
+            src={image.src} 
+            width={200}
+            height={200}
+            alt={image.alt}
+            className="galleryImage"
+            priority
+            />
+            <div className="imageTitle"><b>{image.imageHeader}</b> {image.imageText}</div>
+            </div>
+        ))}
+        {/* <div>
+            <video width="400px" height="500px" preload="none" className="galleryImage" muted autoPlay loop playsInline>
+                <source src="/gallery/mm/VideoMM.mp4" type="video/mp4" />
+                Your browser does not support the video tag.
+            </video>
+            <div className="imageTitle"><b>VICTORIA</b> SANDALS</div>
+        </div> */}
+        </div>
+    </>
+    )
+}
