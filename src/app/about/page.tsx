36ee0265ba384c176a9d243966569e82f8f05680
kstrepo1 <kail.terepai@griffithuni.edu.au>
2025-10-20 00:41:33 +1000

    updates

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,15 +4,71 @@ import Header from "../coreHeader";
 import Image from "next/image";
 import { useState, useEffect } from "react";
 import Link from "next/link";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
+// Function Code:
+//     ---------------
+//     gsap.from(animatedText, {
+//         scrollTrigger: {
+//           trigger: animatedText,
+//           start: "top 85%",
+//           end: "top 35%",
+//           scrub: true,
+//         },
+        // skewX: 45,
+        // opacity: 0,
+        // duration: 2,
+//       });
+
+//     HTML Code:
+//     ---------------
+//     <section class="container-fluid vh-100 d-flex align-items-center justify-content-center position-relative effect-section" data-effect="skew-in">
+//           <button class="btn btn-dark position-absolute top-0 end-0 m-3 copy-btn">Copy Code</button>
+//           <div class="row w-100">
+//             <div class="col-12 text-center">
+//               <h1 class="animated-text" style="translate: none; rotate: none; scale: none; opacity: 0.5759; transform: translate3d(0px, 0px, 0px) skew(19.0843deg, 0deg);">Skew-In Effect</h1>
+//             </div>
+//           </div>
+//         </section>
+
+
 
 
 export default function About() {
+
+    useGSAP(() => { 
+            gsap.to('.aboutText', {
+                scrollTrigger: {
+                    trigger: '.aboutText',  
+                    start: "top 80%",
+                    end: "top 30%",
+                    scrub: false,
+                },
+                skewX: 45,
+                opacity: 0,
+                duration: 2,
+            });
+
+
+
+    });
 
 
     return(
         <>
         <Header/>
-        <h1>About</h1><hr className="white-line"/></>
+        <h1>About</h1><hr className="white-line"/>
+        <div className="aboutText" style={{translate: "none", rotate: "none", scale: "none", opacity: 0.5759, transform: "translate3d(0px, 0px, 0px) skew(19.0843deg, 0deg)"}}>
+        Tall Poppy Studios is a Melbourne-based creative studio led by Mads Russell. 
+        
+        Rooted in photography, design, and brand communication, we help brands craft thoughtful, visually driven narratives that feel authentic and intentional.
+        Our work lives at the intersection of creativity and clarity, from concepting shoots and developing digital campaigns to refining the details that make a brand cohesive. We've collaborated with brands across design, lifestyle, and culture, bringing a hands-on, collaborative approach to every project.
+        At Tall Poppy Studios, we believe in creativity that feels authentic — less about perfection, more about presence.
+        
+        </div>
+
+        </>
         
     )
 }
--- src/app/gallery/sten.tsx
+++ src/app/gallery/sten.tsx
@@ -43,26 +43,26 @@ export default function Sten(){
     });
 
     const images1 = [
-        {id:0, src:"/gallery/sten/STEN HAPPY HOUSE 1.jpg", alt: "Morimoto 1"},
-        {id:2, src: "/gallery/sten/STEN HAPPY HOUSE 2.jpg", alt: "Morimoto 2"},
-        {id:3, src: "/gallery/sten/STEN HAPPY HOUSE 3.jpg", alt: "Morimoto 3"},
-        {id:4, src: "/gallery/sten/STEN HAPPY HOUSE 4.jpg", alt: "Morimoto 4"},
-        {id:5, src: "/gallery/sten/STEN HAPPY HOUSE 5.jpg", alt: "Morimoto 5"},
-        {id:6, src: "/gallery/sten/STEN HAPPY HOUSE 6.jpg", alt: "Morimoto 6"},
-        {id:7, src: "/gallery/sten/STEN HAPPY HOUSE 7.jpg", alt: "Morimoto 7"},
-        {id:8, src: "/gallery/sten/STEN HAPPY HOUSE 8.jpg", alt: "Morimoto 8"},
-        {id:9, src: "/gallery/sten/STEN HAPPY HOUSE 9.jpg", alt: "Morimoto 9"},
-        {id:10, src: "/gallery/sten/STEN HAPPY HOUSE 10.jpg", alt: "Morimoto 10"}
+        {id:0, src:"/gallery/sten/STEN HAPPY HOUSE 1.jpg", alt: "Sten 1", imageHeader:"", imageText:""},
+        {id:2, src: "/gallery/sten/STEN HAPPY HOUSE 2.jpg", alt: "Sten 2", imageHeader:"", imageText:""},
+        {id:3, src: "/gallery/sten/STEN HAPPY HOUSE 3.jpg", alt: "Sten 3", imageHeader:"", imageText:""},
+        {id:4, src: "/gallery/sten/STEN HAPPY HOUSE 4.jpg", alt: "Sten 4", imageHeader:"", imageText:""},
+        {id:5, src: "/gallery/sten/STEN HAPPY HOUSE 5.jpg", alt: "Sten 5", imageHeader:"", imageText:""},
+        {id:6, src: "/gallery/sten/STEN HAPPY HOUSE 6.jpg", alt: "Sten 6", imageHeader:"", imageText:""},
+        {id:7, src: "/gallery/sten/STEN HAPPY HOUSE 7.jpg", alt: "Sten 7", imageHeader:"", imageText:""},
+        {id:8, src: "/gallery/sten/STEN HAPPY HOUSE 8.jpg", alt: "Sten 8", imageHeader:"", imageText:""},
+        {id:9, src: "/gallery/sten/STEN HAPPY HOUSE 9.jpg", alt: "Sten 9", imageHeader:"", imageText:""},
+        {id:10, src: "/gallery/sten/STEN HAPPY HOUSE 10.jpg", alt: "Sten 10", imageHeader:"", imageText:""}
     ]
 
         const images2 = [
-        {id:0, src:"/gallery/sten/STILL LIFE 1.jpg", alt: "Morimoto 1"},
-        {id:2, src: "/gallery/sten/STILL LIFE 2.jpg", alt: "Morimoto 2"},
-        {id:3, src: "/gallery/sten/STILL LIFE 3.jpg", alt: "Morimoto 3"},
-        {id:4, src: "/gallery/sten/STILL LIFE 4.jpg", alt: "Morimoto 4"},
-        {id:5, src: "/gallery/sten/STILL LIFE 5.jpg", alt: "Morimoto 5"},
-        {id:6, src: "/gallery/sten/STILL LIFE 6.jpg", alt: "Morimoto 6"},
-        {id:7, src: "/gallery/sten/STILL LIFE 7.jpg", alt: "Morimoto 7"}
+        {id:0, src:"/gallery/sten/STILL LIFE 1.jpg", alt: "Sten 1", imageHeader:"", imageText:""},
+        {id:2, src: "/gallery/sten/STILL LIFE 2.jpg", alt: "Sten 2", imageHeader:"", imageText:""},
+        {id:3, src: "/gallery/sten/STILL LIFE 3.jpg", alt: "Sten 3", imageHeader:"", imageText:""},
+        {id:4, src: "/gallery/sten/STILL LIFE 4.jpg", alt: "Sten 4", imageHeader:"", imageText:""},
+        {id:5, src: "/gallery/sten/STILL LIFE 5.jpg", alt: "Sten 5", imageHeader:"", imageText:""},
+        {id:6, src: "/gallery/sten/STILL LIFE 6.jpg", alt: "Sten 6", imageHeader:"", imageText:""},
+        {id:7, src: "/gallery/sten/STILL LIFE 7.jpg", alt: "Sten 7", imageHeader:"", imageText:""}
     ]
 
     return(
@@ -75,8 +75,8 @@ export default function Sten(){
         
         <div className="galleryImages" style={{ display: 'flex', flexDirection: 'row', gap: '10px', flexWrap: 'wrap', justifyContent:"center"}}>
         {images1.map((image) => (
+            <div key={image.id}>
             <Image 
-            key={image.id}
             src={image.src} 
             width={200}
             height={200}
@@ -85,10 +85,12 @@ export default function Sten(){
             className="galleryImage"
             priority
             />
+            <div className="imageTitle"><b>{image.imageHeader}</b> {image.imageText}</div>
+            </div>
         ))}
         {images2.map((image) => (
+            <div key={image.id}>
             <Image 
-            key={image.id}
             src={image.src} 
             width={200}
             height={200}
@@ -97,6 +99,8 @@ export default function Sten(){
             className="galleryImage"
             
             />
+            <div className="imageTitle"><b>{image.imageHeader}</b> {image.imageText}</div>
+            </div>
         ))}
 
         <video width="400px" height="500px" preload="none" className="galleryImage" muted autoPlay loop playsInline>
