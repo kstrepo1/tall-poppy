ad1ab0265bd0b0b9f438a9741f2c1c2acf013024
kstrepo1 <kail.terepai@griffithuni.edu.au>
2025-10-21 22:07:45 +1000

    Contact us page update

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -15,11 +15,10 @@ export default function Contact() {
         <h1>Contact</h1><hr className="white-line"/>
         
         <center>
-            <p>JUST A GIRL WHO DOES IT ALL</p>
-            <p>PHOTOGRAPHY/DESIGN/CONTENT</p>
             <p><a href="mailto:hi@tallpoppystudios.au">hi@tallpoppystudios.au</a></p>
             <p><a href="https://www.instagram.com/t.allpoppy/">INSTAGRAM</a></p>
-           
+            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScnD3OUabffIkUlFL2mUuVoS2LUdGrT2YK8ht-fDGxU9u_uqA/viewform?embedded=true" width="640" height="725">Loading…</iframe>
+            
         </center> 
               
         
